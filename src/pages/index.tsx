4081ac720c7afbbefdf6bc74fd49adbfbea17941
arman-ag <alighanbari.1994@gmail.com>
2023-03-22 11:34:34 +0330

    style: :lipstick: create background and header menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,28 @@
+import { useState } from 'react';
+import { BiFolderPlus } from 'react-icons/bi';
+import { RiFileAddLine } from 'react-icons/ri';
+import { File } from '../components/File';
 const index = () => {
-  return <div className='w-10 h-10 bg-black'>index</div>;
+  const [file, setFile] = useState({});
+  return (
+    <div className='w-5/12 h-screen m-auto  bg-black'>
+      <div>
+        <div className='bg-gray-300 w-6/12 m-auto flex align-baseline justify-between'>
+          <span className='text-sm font-bold'>Menu</span>
+          <div>
+            <button>
+              <BiFolderPlus />
+            </button>
+            <button onClick={s}>
+              <RiFileAddLine />
+            </button>
+            {Array.map((a, key) => {
+              return <File key={key} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default index;
